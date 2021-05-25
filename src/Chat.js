cbd0307f7d3d64e27a8b80206b0ab76a4bbb6892
Magesh <mageshsk16@gmail.com>
2021-05-25 16:14:10 +0530

    Add entire sidebar

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import { Avatar, IconButton } from "@material-ui/core";
+import { SearchOutlined, MoreVert, AttachFile, InsertEmoticon, Mic } from "@material-ui/icons";
 import "./Chat.css";
 
 function Chat() {
     return (
         <div className="chat">
-            <h2>I am the chat</h2>
+            <div className='chat_header'>
+                <Avatar/>
+            <div className='chat_headerInfo'>
+                <h3>Room Name</h3>
+                <p>LAst seen at...</p>
+            </div>
+            <div className="chat_headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+            </div>
         </div>
     )
 }
