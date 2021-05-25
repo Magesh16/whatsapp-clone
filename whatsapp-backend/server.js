7d5d5347a845dee4d83449c723fc8ed47f2ed83d
Magesh <mageshsk16@gmail.com>
2021-05-25 22:49:27 +0530

    Add GET POST req

--- whatsapp-backend/server.js
+++ whatsapp-backend/server.js
@@ -2,11 +2,14 @@
 //import express from "express";
 const express = require("express"); 
 const mongoose = require("mongoose");
+const Messages = require("./dbMessages.js");
 
 // app config
 const app =express();
 const PORT = process.env.port || 3000
 //middleware
+app.use(express.json());
+
 //DB config
 const connection_url = "mongodb+srv://admin:dfQZF4soYoGf4K9X@cluster0.jvapi.mongodb.net/whatsappDB?retryWrites=true&w=majority"
 mongoose.connect(connection_url,{
@@ -18,6 +21,19 @@ mongoose.connect(connection_url,{
 //????
 //api routes
 app.get('/',(_req,res) => res.status(200).send("hello world"));
+
+app.post('/messages/new', (req,res)=>{
+    const dbMessage = req.body;
+
+    Messages.create(dbMessage, (err,data)=>{
+        if(err){
+            res.status(500).send(err);
+        }
+        else{
+            res.status(201).send(`new message created: \n ${data}`);
+        }
+    })
+})
 
 //listen
 app.listen(PORT,()=>console.log(`Listening on localhost: ${PORT}`));
