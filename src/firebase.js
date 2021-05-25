29821e062f9e89f190f4a29186d72075a059c041
Magesh <mageshsk16@gmail.com>
2021-05-25 13:35:29 +0530

    Add basic setup in firebase and react

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAilGtdGBN6UGMs1VTGBQZPzQnXNOrVF6A",
+    authDomain: "whatsapp-mern-eb497.firebaseapp.com",
+    projectId: "whatsapp-mern-eb497",
+    storageBucket: "whatsapp-mern-eb497.appspot.com",
+    messagingSenderId: "1046960442145",
+    appId: "1:1046960442145:web:0c3dbd06217f11c8cc8c60"
+  };
