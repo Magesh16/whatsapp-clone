b0d6a14d0c07e6f093f6f4c29e32f2f8e0adfef9
Magesh <mageshsk16@gmail.com>
2021-05-26 13:35:16 +0530

    Add pusher-js

--- whatsapp-mern/src/App.js
+++ whatsapp-mern/src/App.js
@@ -1,8 +1,21 @@
+import {React, useEffect} from "react";
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
+import Pusher from "pusher-js";
 
 function App() {
+
+  useEffect(()=>{
+    const pusher = new Pusher('47388a1a360a82e967c9', {
+      cluster: 'eu'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted',(data) =>{
+      alert(JSON.stringify(data));
+    });
+  }, [])
   return (
     <div className="app">
       <div className='app_body'>
