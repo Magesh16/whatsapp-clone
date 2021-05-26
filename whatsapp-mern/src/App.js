ed95e1f5f4a831e50ba29ecf3615f1d6584a26f8
Magesh <mageshsk16@gmail.com>
2021-05-26 14:03:12 +0530

    Add axios to connect

--- whatsapp-mern/src/App.js
+++ whatsapp-mern/src/App.js
@@ -1,10 +1,24 @@
-import {React, useEffect} from "react";
+import {React,useState, useEffect} from "react";
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
 import Pusher from "pusher-js";
+import axios from "./axios";
 
 function App() {
+
+  const [messages, setMessages] = useState([]);
+
+  useEffect(()=>{
+      axios.get('/messages/sync')
+      .then(response=>{
+        console.log(response.data)
+
+      });
+  }, [])
+
+
+
 
   useEffect(()=>{
     const pusher = new Pusher('47388a1a360a82e967c9', {
@@ -15,7 +29,9 @@ function App() {
     channel.bind('inserted',(data) =>{
       alert(JSON.stringify(data));
     });
-  }, [])
+  }, []);
+console.log(messages);
+
   return (
     <div className="app">
       <div className='app_body'>
